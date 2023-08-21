8b1321d6e8e0d89dffe2630663e709a127663862
Benedetto Habitante <benedikt.wohner@gmx.de>
2023-08-21 16:31:27 +0200

    Finish FB_1

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -23,7 +23,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -54,5 +54,3 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
 }
-
-*/
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -29,18 +29,32 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum class TrafficLightPhase 
+{
+    red,
+    green,
+};
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
+    ~TrafficLight();
 
     // getters / setters
 
+
     // typical behaviour methods
+    void waitForGreen();
+    void simulate() override;
+    TrafficLightPhase getCurrentPhase();
 
 private:
+
+    TrafficLightPhase _currentPhase;
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
